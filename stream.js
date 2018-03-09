1b0ac589bd36e9e1d881dde9022ef7aa9e1dae7a
Ashish <ashutalewar@gmail.com>
2018-03-09 13:17:54 +0530

    adding more ids to follow

--- stream.js
+++ stream.js
@@ -13,22 +13,23 @@ const model_definition = new Schema({}, { strict: false });
 const Tweet = mongoose.model('Tweet', model_definition);
 
 
-// client.get('friends/list', function(error, tweets, response) {
-//   if (!error) {
-//     console.log(tweets);
-//   }else{
-//     console.log(error)
-//   }
-// });
+client.get('friends/list', function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }else{
+    console.log(error)
+  }
+});
 
 
 
-client.stream('statuses/filter', {follow: '37034483,284920800,14293310,140798905,116112129'},  function(stream) {
+client.stream('statuses/filter', {follow: '37034483,284920800,14293310,140798905,116112129,194443378,134758540,1697903767'},  function(stream) {
   stream.on('data', function(tweet) {
     // console.log(tweet);
     if(isReply(tweet)){
       console.log('ignoring retweet')
     }else{
+      tweet.timestamp = Date.now()
       Tweet.create(tweet, function(err, res){
         if(!err)
           console.log('tweet saved')
